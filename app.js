75daa013b1d3ddaefe3178100b34e08e6cf25c6b
Wilmer Alex Quispe Quispe <q.wilmer.alex@gmail.com>
2017-05-05 14:58:52 -0400

    Se actualizó el archivo app.js y la ubicación del archivo main.css

--- app.js
+++ app.js
@@ -1,33 +1,38 @@
+// Importamos las dependencias
 var express =     require('express');
 var path =        require('path');
 var favicon =     require('serve-favicon');
 var bodyParser =  require('body-parser');
 
-// RUTAS
+// Importamos los CONTROLADORES de las rutas
 var index = require('./src/routes/index');
 var users = require('./src/routes/users');
 
-// Crea el servidor
+// Declaramos el servidor
 var app = module.exports = express();
-var port = process.env.PORT || 8000;
 
-// Asignación del motor de vistas
+// Configuramos la carpeta que contiene las VISTAS.
 app.set('views', path.join(__dirname, 'src', 'views'));
+// Configuramos el motor de vistas
 app.set('view engine', 'ejs');
 
-// Configura el servidor
-app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// Definimos el tipo de dato de la variable req.body
 app.use(bodyParser.json());
+// Define la libreria que parseará las consultas en la url
 app.use(bodyParser.urlencoded({ extended: false }));
+// Definimos la carpeta que será publica
 app.use(express.static(path.join(__dirname, 'public')));
+// Definimos la ubicación del icono de la aplicación
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
-// Asignación de CONTROLADORES a las RUTAS
+// Asignamos los CONTROLADORES a las rutas
 app.use('/', index);
 app.use('/users', users);
 
+// Definimos el puerto sobre el que se ejecutará la aplicación
+var port = process.env.PORT || 8000
+
 // Ejecutamos el servidor
-if (!module.parent) {
-  app.listen(port, function() {
-    console.log('La aplicación esta ejecutandose en http://localhost:%s/', port);
-  });
-}
+app.listen(port, function() {
+  console.log('La aplicación esta ejecutandose sobre http://localhost:%s/', port);
+});
